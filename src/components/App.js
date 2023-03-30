338cdc0955bbe97fa355b17d49e9b1b2841e499f
Prathamesh karbele <113411026+prathameshkarbele@users.noreply.github.com>
2023-03-30 22:19:20 +0530

    Update App.js
    p1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,64 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import ChildComponent from "./ChildComponent";
 
-const App = () => {
+function App() {
+  const [cartItems, setCartItems] = useState([
+    { id: 1, name: "Item 1", price: 10 },
+    { id: 2, name: "Item 2", price: 20 },
+    { id: 3, name: "Item 3", price: 30 }
+  ]);
+
+  const [itemName, setItemName] = useState("");
+  const [itemPrice, setItemPrice] = useState("");
+
+  const handleAddItem = () => {
+    const newId = cartItems.length + 1;
+    const newItem = { id: newId, name: itemName, price: parseInt(itemPrice) };
+    setCartItems([...cartItems, newItem]);
+    setItemName("");
+    setItemPrice("");
+  };
+
+  const handleNameChange = (event) => {
+    setItemName(event.target.value);
+  };
+
+  const handlePriceChange = (event) => {
+    setItemPrice(event.target.value);
+  };
+
+  const handleRemoveItem = (id) => {
+    setCartItems(cartItems.filter((item) => item.id !== id));
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div class="parent">
+      <h1>Parent Component</h1>
+      <div>
+        <label htmlFor="itemName">Item Name:</label>
+        <input
+          type="text"
+          id="itemName"
+          value={itemName}
+          onChange={handleNameChange}
+        />
+        <label htmlFor="itemPrice">Item Price:</label>
+        <input
+          type="number"
+          id="itemPrice"
+          value={itemPrice}
+          onChange={handlePriceChange}
+        />
+        <button onClick={handleAddItem}>Add Item</button>
+      </div>
+      <ChildComponent
+        cartItems={cartItems}
+        handleRemoveItem={handleRemoveItem}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
